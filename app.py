938ccf3896fb173f1ced7884f7c3d1b193420d5b
stani <stani1423@gmail.com>
2023-01-04 06:59:47 +0100

    intial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
+import HostService
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+
+@app.route('/host', methods=['POST'])
+@cross_origin(supports_credentials=True)
+def host():
+    if request.method == 'POST':
+        rolesData = request.json
+        print(rolesData)
+        return HostService.host(rolesData);
+    else:
+        return jsonify({'success': 'error'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
